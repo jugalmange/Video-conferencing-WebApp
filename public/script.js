ed50ddf1f8e7f0f72a01c9c16e77630fe3a85dd2
jugalmange <jugalmange03@gmail.com>
2021-04-06 15:36:57 +0530

    cam

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
-const socket = io('/');
+const socket = io('/')
 
 const videoGrid = document.getElementById('video-grid')
-var peer = new Peer()
 
 const myPeer = new Peer(undefined, {
     path: '/peerjs',
@@ -10,28 +9,25 @@ const myPeer = new Peer(undefined, {
 })
 
 let myVideoStream;
-const myVideo = document.createElement('video');
+const myVideo = document.createElement('video')
 myVideo.muted = true;
 const peers = {}
 
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
-}).then((stream) => {
+}).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream)
-
-    socket.on('user-connected', userId => {
-        connectToNewUser(userId, stream)
-        alert('Somebody connected', userId)
-    })
-
-    peer.on('call', call => {
+    myPeer.on('call', call => {
         call.answer(stream)
         const video = document.createElement('video')
         call.on('stream', userVideoStream => {
             addVideoStream(video, userVideoStream)
         })
+    })
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
     })
 
 
@@ -50,16 +46,16 @@ navigator.mediaDevices.getUserMedia({
     })
 })
 
-socket.on('user-disconnected', (userId) => {
+socket.on('user-disconnected', userId => {
     if(peers[userId]) peers[userId].close()
 })
 
-peer.on('open', id => {
+myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id);
 })
 
 
-const connectToNewUser = (userId, stream) => {
+function connectToNewUser(userId, stream){
     const call = myPeer.call(userId, stream)
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
@@ -72,7 +68,7 @@ const connectToNewUser = (userId, stream) => {
 }
 
 
-const addVideoStream = (video, stream) => {
+function addVideoStream (video, stream){
     video.srcObject = stream
     video.addEventListener('loadedmetadata', () => {
         video.play()
--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const io = require('socket.io')(server);
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
     debug: true,
-})
+});
 const { v4: uuidv4 } = require('uuid')
 
 app.use('/peerjs', peerServer);
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs')
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`)
  
@@ -23,12 +23,12 @@ app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room })
 })
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId)
         socket.to(roomId).broadcast.emit('user-connected', userId);
         socket.on('message', (messages) => {
-            io.to(roomId).emit('createMessage', messages, userId)
+            io.to(roomId).emit('createMessage', messages)
         });
         socket.on('disconnect', () => {
             socket.to(roomId).broadcast.emit('user-disconnected', userId)
